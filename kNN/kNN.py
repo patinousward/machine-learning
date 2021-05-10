9829084ed7f6ba1c2bb75402043029b9ee112dfb
jianfeng.tang <jianfeng.tang@pingcl.com>
2021-05-10 11:00:15 +0800

    update kNN.py

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -20,14 +20,15 @@ def classify0(inX,dataSet,labels,k):
     sqDiffMat = diffMat**2 # **代表乘方，会把矩阵中每个元素乘方
     sqDistances = sqDiffMat.sum(axis = 1) # axis=1表示矩阵所有的行中的元素相加
     distances = sqDistances**0.5 # 开方
-    sortedDisIndicies = distances.argsort() # 将函数中的数据进行大小排序，然后按照其索引进行从小到大排序，这里获取到的是排序后的索引（index）
+    sortedDisIndicies = distances.argsort() # 将x中的元素从小到大排列，提取其对应的index(索引)，然后输出到y，这里获取到的是排序后的索引（index）
     classCount = {}  # 定义一个字典，就是map，key是标签，value是统计这个标签的数量
-    for i in rank(k):
+    for i in range(k):
         voteIlabel = labels[sortedDisIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1 # 默认值给0
-    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)# sorted 默认升序
+    # Python3.5中：iteritems变为items,1是tunple的index，这里表示对统计数量倒排序
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)# sorted 默认升序 获取对象的第1个域的值
     # iteritems 将字典以迭代器的方式返回，而items方法则返回一个列表
     # sorted 返回新的数组
-    return sortedClassCount[0][0]
+    return sortedClassCount[0][0] # 返回最高统计数量的标签
 
 
